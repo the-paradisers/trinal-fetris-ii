e644d95c24c973308d2ffbe5392717e9acf72026
Joseph Knoebel <knoebelja@gmail.com>
2017-09-29 15:10:22 -0400

    Tidal Screen COMPLETE MY DOGGZ

--- game/js/MenuState.js
+++ game/js/MenuState.js
@@ -4,12 +4,19 @@ class MenuState {
   preload() { }
 
   create() {
-    let logo = this.add.image(
-      this.world.centerX, 
-      this.world.centerY, 
-      'logo')
-    
-    logo.anchor.set(.5)
+    // let logo = this.add.image(
+    //   this.world.centerX,
+    //   this.world.centerY,
+    //   'logo')
+
+    let gameName = this.add.text(
+      this.world.centerX,
+      this.world.centerY,
+      'Trinal Fetris II',
+      { fill: 'white' }
+    )
+
+    gameName.anchor.set(0.5)
 
     this.input.onTap.addOnce((pointer) => {
       this.state.start('Game')
